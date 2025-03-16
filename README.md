7c0b5638dbdcdddf53095ddb5cef93d0213d44f9
Rizky Paskalis <rizky.paskalis@photon.com>
2025-03-16 16:17:01 +0700

    initiate the project structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # simple_onboarding_app
+
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://raw.githubusercontent.com/iProov/android/master/maven/")
+        maven(url = "https://raw.githubusercontent.com/iProov/android/beta/maven/")
+    }
+}
+
+rootProject.name = "Simple Onboarding App"
+include(":app")
